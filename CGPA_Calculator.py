5c725ca9dd7f3ab17f386e9e915f688b1d8321b1
bleverbd <bleverbd@gmail.com>
2023-12-25 14:06:06 +0600

    1

--- CGPA_Calculator.py
+++ CGPA_Calculator.py
@@ -8,6 +8,7 @@ if(chosse==1):
     number_list=[]
     letter_grade=[]
     grade_point=[]
+    sum_of_credit_and_grade=0.0
 
     print("Enter Total Course Number")
     total=int(input())
@@ -18,43 +19,60 @@ if(chosse==1):
         if(number_list[i]>=80.0 and number_list[i]<=100.0):
              letter_grade.append("A Plus")
              grade_point.append(4.0)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
+
         elif(number_list[i]>=75.0 and number_list[i]<80.0):
              letter_grade.append("A Regular")
              grade_point.append(3.75)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=70.0 and number_list[i]<75.0):
              letter_grade.append("A Minus")
              grade_point.append(3.50)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=65.0 and number_list[i]<70.0):
              letter_grade.append("B Plus")
              grade_point.append(3.25)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=60.0 and number_list[i]<65.0):
              letter_grade.append("B Regular")
              grade_point.append(3.00)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=55.0 and number_list[i]<60.0):
              letter_grade.append("B Minus")
              grade_point.append(2.75)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=50.0 and number_list[i]<55.0):
              letter_grade.append("C Plus")
              grade_point.append(2.50)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=45.0 and number_list[i]<50.0):
              letter_grade.append("C Regular")
              grade_point.append(2.25)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]>=40.0 and number_list[i]<45.0):
              letter_grade.append("D")
              grade_point.append(2.00)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
         elif(number_list[i]<40.0):
              letter_grade.append("F")
              grade_point.append(0.00)
+             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
+    
+    sumation_of_credit=sum(credit_list)
+    gpa=sum_of_credit_and_grade/sumation_of_credit
+    print("Course Name \t Credit Hours \t Letter Grade \t Grade Point")
+    for i in range(0,total):
+         print("Course_{}\t\t{}\t\t{}\t\t{}".format(i+1,credit_list[i],letter_grade[i],grade_point[i]))
+    
+    print("\n\t\t\t\t\t\t\tGPA:{}".format(gpa))
+
+
+
+
         
 
 
 
-
-
-
-
-print(letter_grade)
-print(grade_point)
 
 
     
