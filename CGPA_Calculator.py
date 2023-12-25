c2154b886fcce90846dea9ec6032b9cd6bda9be1
bleverbd <bleverbd@gmail.com>
2023-12-25 11:59:25 +0600

    1

--- CGPA_Calculator.py
+++ CGPA_Calculator.py
@@ -1,1 +1,7 @@
-print("Hellow")
+print("########### Wellcome To My CGPA Calculator ############")
+print("\n 1.Press 1 to Calculate with Cource \n 2.Press 2 to Clculate With Semester")
+
+print("Enter you chosse:")
+chosse=int(input())
+if(chosse==1):
+    
