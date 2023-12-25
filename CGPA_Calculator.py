9858e81f5e463deb49df218f79fc51756f9612a0
bleverbd <bleverbd@gmail.com>
2023-12-25 22:12:34 +0600

    1

--- CGPA_Calculator.py
+++ CGPA_Calculator.py
@@ -4,7 +4,13 @@ print("\n 1.Press 1 to Calculate with Cource \n 2.Press 2 to Clculate With Semes
 print("Enter you chosse:")
 chosse=int(input())
 if(chosse==1):
+    print("Enter The Previous Credit Earned: ")
+    previous_credit_earned=float(input())
+    print("Enter The Previous CGPA: ")
+    previous_cgpa=float(input())
+
     credit_list=[]
+    review_list=[]
     number_list=[]
     letter_grade=[]
     grade_point=[]
@@ -56,15 +62,53 @@ if(chosse==1):
         elif(number_list[i]<40.0):
              letter_grade.append("F")
              grade_point.append(0.00)
-             sum_of_credit_and_grade=sum_of_credit_and_grade+credit_list[i]*grade_point[i]
+             review_list.append(credit_list[i])
+             
     
-    sumation_of_credit=sum(credit_list)
-    gpa=sum_of_credit_and_grade/sumation_of_credit
+    sumation_of_earned_credit=sum(credit_list)-sum(review_list)
+    gpa=sum_of_credit_and_grade/sumation_of_earned_credit
     print("Course Name \t Credit Hours \t Letter Grade \t Grade Point")
     for i in range(0,total):
          print("Course_{}\t\t{}\t\t{}\t\t{}".format(i+1,credit_list[i],letter_grade[i],grade_point[i]))
     
-    print("\n\t\t\t\t\t\t\tGPA:{}".format(gpa))
+    print("\n\t\t\t\t\t\t\tGPA: {:0.2f}".format(gpa))
+    print("Registered Credit Hours in this Semester: {}".format(sum(credit_list)))
+    print("Credit Hours Earned in this Semester: {}".format(sumation_of_earned_credit))
+    print("Total Credit Hours Earned: {}".format(previous_credit_earned+sumation_of_earned_credit))
+    cgpa=(((previous_cgpa*previous_credit_earned)+(sumation_of_earned_credit*gpa))/(previous_credit_earned+sumation_of_earned_credit))
+    print("CGPA: {:0.2f}".format(cgpa))
+
+
+
+elif(chosse==2):
+      credit_earned=[]
+      grade_earned=[]
+      sum_of_gpa=0
+      print("Enter Total Semester Number")
+      total=int(input())
+      for i in range(0,total):
+           print("Enter Your Semester_{} Credit and Grade".format(i+1))
+           credit_earned.append(float(input()))
+           grade_earned.append(float(input()))
+           sum_of_gpa= sum_of_gpa + credit_earned[i]*grade_earned[i]
+     
+
+      print("Toral Semester: {}".format(total))
+      print("Total Credit: {}".format(sum(credit_earned)))
+      print("Total Grade: {:0.2f}".format(sum(grade_earned)))
+      print("CGPA {:0.2f}".format(sum_of_gpa/sum(credit_earned)))
+
+
+
+
+
+
+
+
+
+    
+
+
 
 
 
